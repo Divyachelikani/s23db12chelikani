3e8d59a5b76d52307cd85c673a0f4958750522bc
Divyachelikani <123126195+Divyachelikani@users.noreply.github.com>
2023-04-24 18:21:48 -0500

    done

--- routes/biscuit.js
+++ routes/biscuit.js
@@ -2,6 +2,19 @@ var express = require('express');
 const biscuit_controlers= require('../controllers/biscuit');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
+  
+
+
 /* GET costumes */
 router.get('/',biscuit_controlers.biscuit_view_all_Page );
 router.get('/detail', biscuit_controlers.biscuit_view_one_Page);
@@ -9,6 +22,11 @@ router.get('/create', biscuit_controlers.biscuit_create_Page);
 router.get('/update', biscuit_controlers.biscuit_update_Page);
 router.get('/delete', biscuit_controlers.biscuit_delete_Page);
 
+/* GET update costume page */
+router.get('/update', biscuit_controlers.biscuit_update_Page);
+
+/* GET update costume page */
+router.get('/update', secured,biscuit_controlers.biscuit_update_Page);
 
 module.exports = router;
 
